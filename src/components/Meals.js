b73d6c5cf1fcba10a346d2218d63c973719c35d3
Eleriin Kurs <eleriin.kurs@voco.ee>
2025-04-04 09:53:48 +0300

    Header ja Meals komponentite kasutusele votmine app.js-is

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,10 +1,14 @@
+import React from "react"
+
 const Meals = () => {
     return (
         <ul id="meals">
+            <h2>create a list of meals</h2>
             { 
                 // list of meals
             }
         </ul>
+        
     )
 }
 
