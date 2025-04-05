4f37d6945100fb82a7a9f163f0e06c29450cf683
Eleriin Kurs <eleriin.kurs@voco.ee>
2025-04-05 11:41:55 +0300

    Modali loomine koos close nupuga

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext,useState } from 'react'
 import logo from '../assets/logo.jpg'
 import Button from './UI/Button'
 import CartContext from '../store/CartContext'
+import Modal from './UI/Modal'
 
 const Header = () => {
     const cartCtx = useContext(CartContext);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openCartHandler = () =>{
+        setIsModalOpen(true);
+    }
+    const closeCartHandler = ()=>{
+        setIsModalOpen(false);
+    }
 
     return (
         <header id="main-header">
@@ -13,8 +22,18 @@ const Header = () => {
                 <h1>React Food Order App</h1>
             </div>
             <nav>
-            <button textOnly>Cart ({cartCtx.items.length})</button>
+            <button textOnly onClick={openCartHandler}>
+                Cart ({cartCtx.items.length})
+                </button>
             </nav>
+
+            <Modal open={isModalOpen} onClose={closeCartHandler}>
+                <h2>Your Cart</h2>
+                <p>Here will be the cart itemes...</p>
+                <div className='modal-action'>
+                    <Button textOnly onClick={closeCartHandler}>Close</Button>
+                </div>
+            </Modal>
         </header>
     )
 }
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,34 @@
-const Modal = (props) => {
+import React, {useEffect, useRef} from "react"
+import "../../index.css";
+import { use } from "react";
+
+const Modal = ({open, onClose, children}) => {
+    const dialogRef = useRef();
+
+    useEffect (()=> {
+        const dialog = dialogRef.current;
+
+        if(open){
+            if(!dialog.open){
+                dialog.showModal();
+            }
+        } else {
+            if(dialog.open){
+                dialog.close();
+            }
+        }
+        const handleCancel = (event) =>{
+            event.preventDefault();
+            onClose();
+        };
+        dialog.addEventListener('Cancel',handleCancel);
+
+        return()=> dialog.removeEventListener('cancel', handleCancel);
+    },[open,onClose]);
+
     return ( 
-        <dialog>
-            {props.children}
+        <dialog ref={dialogRef} className="modal">
+            {children}
         </dialog>
         
     )
